affaa0bf5742497ae4f91b14022dcba80fce3177
Yusong Wang <admin@wangyusong.com>
2022-02-07 17:07:18 +0800

    修改一个bug

--- src/views/server/index.vue
+++ src/views/server/index.vue
@@ -201,7 +201,146 @@
         </el-table>
       </div>
 
-      <div v-else>Now you don't</div>
+      <div v-else>
+        <el-row :gutter="40">
+          <el-col :span="6">
+            <div class="grid-content bg-purple">
+              <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                  <span>在线矿工</span>
+                </div>
+                <div class="text item">{{ current_server.online }}</div>
+              </el-card>
+            </div>
+          </el-col>
+
+          <el-col :span="6">
+            <div class="grid-content bg-purple">
+              <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                  <span>总算力</span>
+                </div>
+                <div class="text item">{{ current_server.total_hash }}</div>
+              </el-card>
+            </div>
+          </el-col>
+
+          <el-col :span="6">
+            <div class="grid-content bg-purple">
+              <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                  <span>总/接受/拒绝/百分比</span>
+                </div>
+                <div class="text item">
+                  {{ current_server.accept_index }}/{{
+                    current_server.share_index
+                  }}/{{ current_server.reject_index }}/{{
+                    current_server.rate
+                  }}%
+                </div>
+              </el-card>
+            </div>
+          </el-col>
+
+          <el-col :span="6">
+            <div class="grid-content bg-purple">
+              <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                  <span>端口TCP/SSL/加密</span>
+                </div>
+                <div class="text item">
+                  {{ current_server.config.tcp_port }}/{{
+                    current_server.config.ssl_port
+                  }}/{{ current_server.config.encrypt_port }}
+                </div>
+              </el-card>
+            </div>
+          </el-col>
+          <div style="margin-top: 30px"></div>
+          <el-col :span="6">
+            <div class="grid-content bg-purple">
+              <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                  <span>运行模式</span>
+                </div>
+                <div class="text item">
+                  <div v-if="current_server.config.share == 0">纯中转</div>
+                  <div v-if="current_server.config.share == 1">按比例抽水</div>
+                  <div v-if="current_server.config.share == 2">统一钱包</div>
+                </div>
+              </el-card>
+            </div>
+          </el-col>
+
+          <el-col :span="6">
+            <div class="grid-content bg-purple">
+              <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                  <span>池</span>
+                </div>
+                <div class="text item">
+                  <div>{{ current_server.config.pool_address[0] }}</div>
+                </div>
+              </el-card>
+            </div>
+          </el-col>
+        </el-row>
+
+        <div style="margin-top: 30px"></div>
+
+        <el-table
+          v-loading="listLoading"
+          :data="current_server.workers"
+          element-loading-text="Loading"
+          border
+          fit
+          highlight-current-row
+        >
+          <el-table-column align="center" label="钱包">
+            <template slot-scope="scope">
+              {{ scope.row.worker_wallet }}
+            </template>
+          </el-table-column>
+          <el-table-column label="矿工名" align="center" width="400">
+            <template slot-scope="scope">
+              {{ scope.row.worker_name }}
+            </template>
+          </el-table-column>
+          <el-table-column label="报告算力" width="160" align="center">
+            <template slot-scope="scope">
+              <span>{{ scope.row.hash }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column label="总工作份额" width="160" align="center">
+            <template slot-scope="scope">
+              {{ scope.row.share_index }}
+            </template>
+          </el-table-column>
+
+          <el-table-column
+            class-name="status-col"
+            label="接受份额"
+            width="160"
+            align="center"
+          >
+            <template slot-scope="scope">
+              {{ scope.row.accept_index }}
+            </template>
+          </el-table-column>
+
+          <el-table-column
+            align="center"
+            prop="created_at"
+            label="拒绝份额"
+            width="160"
+          >
+            <template slot-scope="scope">
+              <i class="el-icon-time" />
+              <span>{{ scope.row.invalid_index }}</span>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
     </div>
   </div>
 </template>
