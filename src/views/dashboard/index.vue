dcd82a06061397ae38261cafa5aaf223ae87b63e
Yusong Wang <admin@wangyusong.com>
2022-02-07 14:18:49 +0800

    修改一个bug

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -213,26 +213,26 @@ export default {
       overview: {},
       activities: [
         {
-          content: "支持使用图标",
-          timestamp: "2018-04-12 20:46",
+          content: "0.2.2 Web界面版本发布",
+          timestamp: "2022-02-06",
           size: "large",
           type: "primary",
           icon: "el-icon-more",
         },
-        {
-          content: "支持自定义颜色",
-          timestamp: "2018-04-03 20:46",
-          color: "#0bbd87",
-        },
-        {
-          content: "支持自定义尺寸",
-          timestamp: "2018-04-03 20:46",
-          size: "large",
-        },
-        {
-          content: "默认样式的节点",
-          timestamp: "2018-04-03 20:46",
-        },
+        // {
+        //   content: "支持自定义颜色",
+        //   timestamp: "2018-04-03 20:46",
+        //   color: "#0bbd87",
+        // },
+        // {
+        //   content: "支持自定义尺寸",
+        //   timestamp: "2018-04-03 20:46",
+        //   size: "large",
+        // },
+        // {
+        //   content: "默认样式的节点",
+        //   timestamp: "2018-04-03 20:46",
+        // },
       ],
     };
   },
